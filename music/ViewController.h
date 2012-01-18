2171fbc28059d7eb4491d2edf6f5190bc3390cc7
qianfeng <aaaaa@amatoMacBook-Air-67.local>
2012-01-18 09:31:19 +0800

    Initial Commit

---
+++ music/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  music
+//
+//  Created by a a a a a on 12-1-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
